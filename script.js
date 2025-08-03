3517b87ff19eae207fa7beff172fb9eb8c66455d
Siwoo Kim <siw.kim@outlook.com>
2025-08-03 15:32:03 +0900

    add various config parameters

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
+//
+// lattice boltzmann method
+// basic reference implementation
+//
+
+// config
+
+// lattice config
+// one pixel per cell
 let widthImg  = 500;
 let heightImg = 500;
+let scaleCell = 1;
 
-let p = new Float32Array(widthImg * heightImg);
+// time step
+let dt = 0.01;
 
-let canv = document.getElementById('disp');
-let ctx  = canv.getContext('2d');
+// kinematic viscosity
+let nu = 1;
+
+// reference density
+let rho = 1;
+
+// norm particle density, relative to ref density
+// 9 directions:
+// 6 2 5
+// 3 0 1
+// 7 4 8
+let p = new Float32Array(9 * widthImg * heightImg);
+
+// weights for collision operator
+let w = new Float32Array([
+     1,  0,
+     0,  1,
+    -1,  0,
+     0, -1,
+     1,  1,
+    -1,  1
+    -1, -1,
+     1, -1
+]);
+
+// init
+
+// calculate speed of sound with lattice config, tau using kinematic viscosity
+let c   = scaleCell / dt / Math.sqrt(3); // notice not simply scale divided by time
+let tau = nu / (c * c * dt) + 0.5;
+
+let disp = document.getElementById('disp');
+let ctx  = disp.getContext('2d');
 let img  = ctx.createImageData(widthImg, heightImg);
 
 console.log('init');
